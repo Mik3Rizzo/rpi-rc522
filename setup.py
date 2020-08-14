37adbd8d24476236f4312cf7d7d3ded8209e6870
Zontex <Zontex@users.noreply.github.com>
2020-08-14 18:05:13 +0800

    package name updated

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'RC522-Python')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'RC522_Python')))
 
-from RC522-Python import __version__  # flake8: noqa
+from RC522_Python import __version__
 sys.path.pop(0)
 
 setup(
@@ -15,18 +15,22 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     version=__version__,
+    download_url = 'https://github.com/STEMinds/RC522-Python/archive/1.0.0.tar.gz',
+    keywords = ['python', 'raspberry-pi', 'RC522', 'RFID', 'NFC', 'SPI'],
     description='Raspberry Pi Python library for SPI RFID RC522 module.',
     long_description='Raspberry Pi Python library for SPI RFID RC522 module.',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU License',
         'Operating System :: OS Independent',
         'Topic :: Software Development',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     author='STEMinds',
     author_email='contact@steminds.com',
