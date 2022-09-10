825253718df5ff930d2726682aba5a1b716d6a2c
Mik3Rizzo <mik3.rizzo@gmail.com>
2022-09-10 11:19:06 +0200

    Added classes in __init__

--- rpi_rc522/__init__.py
+++ rpi_rc522/__init__.py
@@ -3,5 +3,6 @@ __version__ = "1.0.0"
 try:
     from .rfid_reader import RFIDReader
     from .rfid_util import RFIDUtil
+    from .rfid_tag import RFIDTag
 except RuntimeError:
     print("Must be used on Raspberry Pi")
